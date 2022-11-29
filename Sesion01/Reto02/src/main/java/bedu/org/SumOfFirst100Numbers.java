3678472b15c1a34e190b68dd1e72283cac5caf42
Emmanuel Acevedo Diaz <eacevedod@uaemex.mx>
2022-11-28 22:46:16 -0600

    Reto 02 Sesion 01

---
+++ Sesion01/Reto02/src/main/java/bedu/org/SumOfFirst100Numbers.java
@@ -0,0 +1,13 @@
+package bedu.org;
+
+public class SumOfFirst100Numbers {
+    public static void main(String [] args) {
+        int total = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            total += i;
+        }
+
+        System.out.println("La suma de los primeros 100 números es: " + total);
+    }
+}
