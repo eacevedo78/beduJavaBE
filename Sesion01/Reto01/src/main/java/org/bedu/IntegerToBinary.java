43abc1c339c46821fe5501617329218fd6b80dbe
Emmanuel Acevedo Diaz <eacevedod@uaemex.mx>
2022-11-28 22:28:22 -0600

    Sesion 01

---
+++ Sesion01/Reto01/src/main/java/org/bedu/IntegerToBinary.java
@@ -0,0 +1,11 @@
+package org.bedu;
+
+public class IntegerToBinary {
+    public static void main(String [] args) {
+        int number = 20;
+
+        String binary = Integer.toBinaryString(number);
+
+        System.out.println("El número entero " + number + " en binario es: " + binary);
+    }
+}
