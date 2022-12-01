5c9a6552cccc5d67c9274b0bf0bb84bae9377129
Emmanuel Acevedo Diaz <eacevedod@uaemex.mx>
2022-11-30 21:53:52 -0600

    Calculadora con opcion de dividir

---
+++ Sesion02/02Comportamiento-Calculadora/ComandoDividir.java
@@ -0,0 +1,9 @@
+public class ComandoDividir extends ComandoCalculadora implements Comando {
+  public ComandoDividir(float acumulador, float valor) {
+    super(acumulador, valor);
+  }
+
+  public float ejecutar() {
+    return acumulador / valor;
+  }
+}
